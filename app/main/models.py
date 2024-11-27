30cd39ddf19d39f61b0fdaa2cab3cbfa61e76512
Kobby S. Arthur <ser-arthur@kobbys-MacBook-Pro.local>
2024-11-27 17:24:21 +0000

    Update db model

--- app/main/models.py
+++ app/main/models.py
@@ -31,7 +31,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(5000), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
     def set_password(self, password):
